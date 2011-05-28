07f219dfe08100625114019b351eb9ead802fbcb
Tijs Verkoyen <tijs@verkoyen.eu>
2011-05-28 18:16:12 +0200

    Added tests

---
+++ tests/campaign_commander_member_test.php
@@ -0,0 +1,211 @@
+<?php
+
+require_once 'config.php';
+require_once '../campaign_commander_member.php';
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * CampaignCommanderMember test case.
+ */
+class CampaignCommanderMemberTest extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var CampaignCommanderMember
+	 */
+	private $campaignCommanderMember;
+
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->campaignCommanderMember = new CampaignCommanderMember(LOGIN, PASSWORD, KEY);
+	}
+
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		$this->campaignCommanderMember = null;
+
+		parent::tearDown();
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getTimeOut()
+	 */
+	public function testGetTimeOut()
+	{
+		$this->campaignCommanderMember->setTimeOut(5);
+		$this->assertEquals(5, $this->campaignCommanderMember->getTimeOut());
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getUserAgent()
+	 */
+	public function testGetUserAgent()
+	{
+		$this->campaignCommanderMember->setUserAgent('testing/1.0.0');
+		$this->assertEquals('PHP Campaign Commander Member/' . CampaignCommanderMember::VERSION . ' testing/1.0.0', $this->campaignCommanderMember->getUserAgent());
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->descMemberTable()
+	 */
+	public function testDescMemberTable()
+	{
+		$this->assertType('array', $this->campaignCommanderMember->descMemberTable());
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getMemberByEmail()
+	 */
+	public function testGetMemberByEmail()
+	{
+		$var = $this->campaignCommanderMember->getMemberByEmail('spam@verkoyen.eu');
+
+		$this->assertArrayHasKey('EMVDOUBLON', $var);
+		$this->assertArrayHasKey('EMAIL', $var);
+		$this->assertArrayHasKey('DATEUNJOIN', $var);
+		$this->assertArrayHasKey('MEMBER_ID', $var);
+		$this->assertArrayHasKey('DATEJOIN', $var);
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getMemberById()
+	 */
+	public function testGetMemberById()
+	{
+		$var = $this->campaignCommanderMember->getMemberById('1043875306833');
+
+		$this->assertArrayHasKey('EMVDOUBLON', $var);
+		$this->assertArrayHasKey('EMAIL', $var);
+		$this->assertArrayHasKey('DATEUNJOIN', $var);
+		$this->assertArrayHasKey('MEMBER_ID', $var);
+		$this->assertArrayHasKey('DATEJOIN', $var);
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getListMembersByObj()
+	 */
+	public function testGetListMembersByObj()
+	{
+		$this->assertType('array', $this->campaignCommanderMember->getListMembersByObj(array('dynContent' => array(), 'memberUID' => 'FIRSTNAME:jan')));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getListMembersByPage()
+	 */
+	public function testGetListMembersByPage()
+	{
+		$this->assertType('array', $this->campaignCommanderMember->getListMembersByPage(1));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->insertMember()
+	 */
+	public function testInsertMember()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->insertMember('spam@verkoyen.eu'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->updateMember()
+	 */
+	public function testUpdateMember()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->updateMember('spam@verkoyen.eu', 'FIRSTNAME', 'spam'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->insertOrUpdateMemberByObj()
+	 */
+	public function testInsertOrUpdateMemberByObj()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->insertOrUpdateMemberByObj(array('FIRSTNAME' => 'MARK'), 'spam@verkoyen.eu'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->updateMemberByObj()
+	 */
+	public function testUpdateMemberByObj()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->updateMemberByObj(array('FIRSTNAME' => 'MARK'), 'spam@verkoyen.eu'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->getMemberJobStatus()
+	 */
+	public function testGetMemberJobStatus()
+	{
+		$var = $this->campaignCommanderMember->updateMember('spam@verkoyen.eu', 'FIRSTNAME', time());
+		$var = $this->campaignCommanderMember->getMemberJobStatus($var);
+		$this->assertType('string', $var);
+		$this->assertEquals('Insert', $var);
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->unjoinMemberByEmail()
+	 */
+	public function testUnjoinMemberByEmail()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->unjoinMemberByEmail('spam@verkoyen.eu'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->unjoinMemberById()
+	 */
+	public function testUnjoinMemberById()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->unjoinMemberById('1045452342012'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->unjoinMemberByObj()
+	 */
+	public function testUnjoinMemberByObj()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->unjoinMemberByObj(array('dynContent' => array(), 'memberUID' => 'email:spam@verkoyen.eu')));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->rejoinMemberByEmail()
+	 */
+	public function testRejoinMemberByEmail()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->rejoinMemberByEmail('spam@verkoyen.eu'));
+	}
+
+
+	/**
+	 * Tests CampaignCommanderMember->rejoinMemberById()
+	 */
+	public function testRejoinMemberById()
+	{
+		$this->assertType('string', $this->campaignCommanderMember->rejoinMemberById('1045452342012'));
+	}
+}
+
---
+++ tests/index.php
@@ -0,0 +1,43 @@
+<?php
+
+// require
+require_once 'config.php';
+require_once '../campaign_commander_member.php';
+
+// create instance
+$ccm = new CampaignCommanderMember(LOGIN, PASSWORD, KEY);
+
+//$response = $ccm->descMemberTable();
+//$response = $ccm->getMemberByEmail('specialchars@verkoyen.eu');
+//$response = $ccm->getMemberById('1043875306833');
+//$response = $ccm->getListMembersByObj(array(
+//	'dynContent' => array(),
+//	'memberUID' => 'FIRSTNAME:jan'
+//));
+//$response = $ccm->getListMembersByPage(1);
+//$response = $ccm->insertMember('spam@verkoyen.eu');
+//$response = $ccm->updateMember('spam@verkoyen.eu', 'FIRSTNAME', 'spam');
+//$response = $ccm->insertOrUpdateMemberByObj(array('FIRSTNAME' => 'MARK'), 'spam@verkoyen.eu');
+//$response = $ccm->updateMemberByObj(array('FIRSTNAME' => 'MARK'), 'spam@verkoyen.eu');
+//$response = $ccm->getMemberJobStatus('134912671');
+//$response = $ccm->unjoinMemberByEmail('spam@verkoyen.eu');
+//$response = $ccm->unjoinMemberById('1045452342012');
+//$response = $ccm->unjoinMemberByObj(array(
+//	'dynContent' => array(),
+//	'memberUID' => 'email:spam@verkoyen.eu'
+//));
+//$response = $ccm->rejoinMemberByEmail('spam@verkoyen.eu');
+//$response = $ccm->rejoinMemberById('1045452342012');
+
+// output (Spoon::dump())
+ob_start();
+var_dump($response);
+$output = ob_get_clean();
+
+// cleanup the output
+$output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+
+// print
+echo '<pre>' . htmlspecialchars($output, ENT_QUOTES, 'UTF-8') . '</pre>';
+
+?>
